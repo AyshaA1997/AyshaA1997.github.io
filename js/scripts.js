73697322abf14c46ee3a8f40d3f8c1dfbba34283
Aysha <aysha.alhafian@eagles.ewu.edu>
2019-03-11 11:28:52 -0700

    addedlightgallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 // Everything OK?
 console.log('OK');
 
+
+//Light Gallery
+$('gallery-1').lightGallery({
+    thumbnail:false,
+    share: false,
+});
+
+$('gallery-2').lightGallery({
+    share:false,
+});
 // Your Awesome Scripts
